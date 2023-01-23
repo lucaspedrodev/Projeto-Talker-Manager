c46858be68d5c86547f47ff88bafa4937ea2327d
Lucas Pedro <rodrigerioo@gmail.com>
2023-01-23 12:30:41 -0300

    requisitos 05 ao 07

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { talkersData, tokenGen } = require('./utils/fsCustom');
-const { emailValidation, passwordValidation } = require('./middlewares/validation');
+const fs = require('fs').promises;
+const { talkersData, tokenGen, writeNewTalkerData, talkerDelete } = require('./utils/fsCustom');
+const { emailValidation, passwordValidation, autoriza, nameValidation, ageValidation,
+talkValidation, watchedAtValidation, rateValidation } = require('./middlewares/validation');
 
 const app = express();
 app.use(express.json());
@@ -37,6 +39,43 @@ app.get('/talker/:id', async (req, res) => {
 app.post('/login', emailValidation, passwordValidation, (_req, res) => {
   res.status(200).json({ token: tokenGen() });
 });
+
+app.post('/talker', autoriza, nameValidation, ageValidation,
+talkValidation, watchedAtValidation, rateValidation, async (req, res) => {
+  const talkers = await talkersData();
+  const newTalker = { id: talkers.length + 1, ...req.body };
+  const writeTalker = await writeNewTalkerData(newTalker);
+
+  res.status(201).json(writeTalker);
+});
+
+app.put('/talker/:id', 
+autoriza,
+nameValidation,
+ageValidation,
+talkValidation,
+watchedAtValidation,
+rateValidation,
+async (req, res) => {
+    const { id } = req.params;
+    const { name, age, talk } = req.body;
+    const talkers = await talkersData();
+
+    const index = talkers.findIndex((element) => element.id === Number(id));
+    talkers[index] = { id: Number(id), name, age, talk };
+    await fs.writeFile('src/talker.json', JSON.stringify(talkers));
+    res.status(200).json(talkers[index]);
+  });
+
+  app.delete('/talker/:id', autoriza, async (req, res) => {
+    const { id } = req.params;
+  
+    const updateData = await talkerDelete(Number(id));
+  
+    console.log(updateData);
+  
+    return res.status(204).end();
+  });
 
 app.listen(PORT, () => {
   console.log('Online');
--- src/utils/fsCustom.js
+++ src/utils/fsCustom.js
@@ -1,23 +1,84 @@
 const fs = require('fs').promises;
+// const path = require('path');
 const crypto = require('crypto');
+
+// const jsonPath = path.resolve(`${__dirname}/talker.json`);
 
 const talkersData = async () => {
     try {
-        const tkData = await fs.readFile('src/talker.json');
-        const tkDataParse = JSON.parse(tkData);
-        return tkDataParse;
+        const data = await fs.readFile('src/talker.json');
+        const tkData = JSON.parse(data);
+        return tkData;
     } catch (error) {
-       console.log(error);
+        console.log(`Erro na leitura : ${error}`);
     }
 };
+
+console.log(talkersData());
 
 const tokenGen = () => {
     const token = crypto.randomBytes(8).toString('hex');
     return token;
 };
 
+// const writeTalkerFile = async (talker) => {
+//     try {
+//       const talkerData = await talkersData();
+//       const newTalker = {
+//         name: talker.name,
+//         age: talker.age,
+//         id: talkerData[talkerData.length - 1].id + 1,
+//         talk: talker.talk,
+//       };
+//       const allTalkers = JSON.stringify([...talkerData, newTalker]);
+//       await fs.writeFile(jsonPath, allTalkers);
+//       return newTalker;
+//       } catch (err) {
+//         return null;s
+//       }
+//   };
+
+// const newTalkerObj = async (talker) => {    
+//     const talkers = await talkersData();
+//     const ids = talkers[talkers.length - 1].id + 1;
+//     const talkerObj = { id: ids, ...talker };
+//     const talkerObjMake = JSON.stringify([...talkers, talkerObj]);
+//     await fs.writeFile(jsonPath, talkerObjMake);
+//     return talkerObjMake;
+// };
+
+// const registerNewTalker = async (newTalker) => {
+//     const talkerList = await talkersData();
+//     const newId = talkerList[talkerList.length - 1].id + 1;
+//     const newObj = { id: newId, ...newTalker };
+//     talkerList.push(newObj);
+//     await fs.writeFile(jsonPath, JSON.stringify(talkerList));
+//     return newObj;
+// };
+
+const writeNewTalkerData = async (talk) => {
+      const data = await talkersData();
+      const newTalkerId = { id: data.length + 1, ...talk };
+      const talkers = JSON.stringify([...data, newTalkerId]);
+
+      await fs.writeFile(('src/talker.json'), talkers);
+
+      return newTalkerId;  
+};
+
+const talkerDelete = async (id) => {
+    const talkers = await talkersData();
+
+    const talkerFilter = talkers.filter((talk) => talk.id !== id);
+
+    const updateData = JSON.stringify(talkerFilter);
+ 
+        await fs.writeFile('src/talker.json', updateData);
+};  
+    
 module.exports = {
     talkersData,
     tokenGen,
-
+    writeNewTalkerData,
+    talkerDelete,
 };
